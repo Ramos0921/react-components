2bec538df520d5c286c7d35cc1c2062f20e0dee1
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-05-26 12:23:33 -0700

    React-components complete

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+var App = ()=>(
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList list ={['beer','whiskey','steak','toilet paper','bacon']}/>
+  </div>
+);
+
+class GroceryListItem extends React.Component{
+
+    constructor(props) {
+      super(props);
+
+
+      // this.state = {
+      //   done: false
+      // };
+      this.state = {
+        check: false
+      };
+
+    };
+    onListItemClick() {
+      this.setState({
+        check: !this.state.check
+      });
+    }
+
+    // onListenItemClick(){
+    //   this.setState({
+    //     done: !this.state.done
+    //   })
+    // };
+
+    render(){
+      let style = {'font-weight': this.state.check ? 'bold' : 'normal' };
+      return (
+      <li style ={style} onClick={this.onListItemClick.bind(this)}>{this.props.list}</li>
+      );
+    };
+};
+
+var GroceryList = (props)=>(
+  <ul>
+    {props.list.map(item =>
+      <GroceryListItem list ={item} />
+      )}
+  </ul>
+);
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
